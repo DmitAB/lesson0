22f70ade55066515cecb27296b0ca10f8f0b2e67
Dmitriy <zhilexpert@gmail.com>
2024-11-25 15:45:09 +0400

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,14 +1,33 @@
+# 1st program
 print('1st program')
-print(9**0.5*5)
+print((9 ** 0.5) * 5)
+# 2nd program
 print('2nd program')
-print(99.9>98.9,1000!=1000.1)
+a = 99.9 > 98.9
+b = 1000 != 1000.1
+print(a, b)
+# 3rd program
 print('3rd program')
+d = (2*2+2)
 print(2*2+2)
+c = (2*(2+2))
 print(2*(2+2))
-print(6==8)
+print(d == c)
+# 4th program
 print('4th program')
-print('123.456')
-print(123.456*10)
-print(type(1234.56))
-print(int(1234.56)/10)
-print(int(1234.56)%10)
+St_ = '123.456'
+St_ = float(St_)
+St_ = (St_*10)
+print(int(St_) % 10)
+
+
+name="Denis"  # это строка "Denis"
+print("Hello "+name)   #  это подстрока для "Denis"
+print("Hello"*5)
+print(name[0])
+print(name[-1])
+print(name[0:3])
+print(name[0:3:2])
+print(name[:3])
+print(name[2:])
+print(name[::-1])
